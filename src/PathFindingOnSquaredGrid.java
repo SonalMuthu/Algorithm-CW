42a6ef11b336496518a48614dc4995a826da7610
Sonal <sonal.2015347@iit.ac.lk>
2017-03-16 22:19:46 +0530

    bug fix 01

--- src/PathFindingOnSquaredGrid.java
+++ src/PathFindingOnSquaredGrid.java
@@ -108,16 +108,6 @@ public class PathFindingOnSquaredGrid {
                 }
             }
 
-
-            if(!isManhat){
-            //checking Top Right Node
-            if (current.x - 1 >= 0 && current.y + 1 < size) {
-                nextNode = gridNode[current.x - 1][current.y + 1];
-                double newDistance = current.distance + diagonalDistance;
-                if (!nextNode.blocked && !nextNode.visited && nextNode.distance > newDistance) {
-                    nextNode.distance = newDistance;
-                    nextNode.parent = current;
-                    queue.add(nextNode);
                 }
             }
 
