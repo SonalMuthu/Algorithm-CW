d5f88c48e792bb0eec0acf482aaa949688a92504
Sonal <sonal.2015347@iit.ac.lk>
2017-04-02 17:00:24 +0530

    bug fix in euclidean

--- src/Main.java
+++ src/Main.java
@@ -69,24 +69,6 @@ public class Main {
 	        System.out.println("ENT
 	        int By2 = in.nextInt();
 			
-	        System.out.println("ENTER y2 FOR B > ");
-	        int Bx2 = in.nextInt();
-			
-	        System.out.println("ENT
-	        int By2 = in.nextInt();
-			    
-	        
-	        int Ax1 = in.nextInt();
-	        
-	        System.out.println("ENTER x2 FOR B > ");
-	        int By2 = in.nextInt();
-	        
-	        System.out.println("ENTER y2 FOR B > ");
-	        int Bx2 = in.nextInt();
-			
-	        System.out.println("ENT
-	        int By2 = in.nextInt();
-			
 	        
 	        int Ax1 = in.nextInt();
 	        
