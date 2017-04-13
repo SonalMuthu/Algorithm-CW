2d3c1c750a5273a495d2132c631587cfc25af0b2
Sonal <sonal.2015347@iit.ac.lk>
2017-04-13 17:33:32 +0530

    bug fix

--- src/Main.java
+++ src/Main.java
@@ -112,20 +112,6 @@ public class Main {
 	        System.out.println("ENTER x2 FOR B > ");
 	        int By2 = in.nextInt();
 	        
-			 System.out.println("ENTER y2 FOR B > ");
-	        int Bx2 = in.nextInt();
-			
-	           
-	        System.out.println("ENTER x2 FOR B > ");
-	        int By2 = in.nextInt();
-	        
-	        int Ax1 = in.nextInt();
-	        
-	        S
-	        
-	        System.out.println("ENTER x2 FOR B > ");
-	        int By2 = in.nextInt();
-	        
 	        System.out.println("ENTER y2 FOR B > ");
 	        int Bx2 = in.nextInt();
 			
